68cb7f549a0deddd377d3cc6f52920ff4c98e9e1
muhammadhamzabutt80@gmail.com <muhammadhamzabutt80@gmail.com>
2020-07-01 15:59:25 +0500

    CRUD About Roles Has Been Implemented

---
+++ IdentityCode/Controllers/RoleManagment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using IdentityCode.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCode.Controllers
+{
+    public class RoleManagment : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public RoleManagment(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> CreateRole(CreateAppRoles model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+              IdentityResult result= await roleManager.CreateAsync(identityRole);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+       
+    }
+}
